84dad93f5e4b2d54e8f5b2ea042ddcd888a54f14
tyler-widdison <tylerperrywiddison@gmail.com>
2025-10-05 17:56:08 -0600

    Remove @nuxtjs/seo module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -64,7 +64,7 @@ export default defineNuxtConfig({
   },
 
   css: [],
-  modules: ["@nuxt/fonts", "vuetify-nuxt-module", "@nuxtjs/seo"],
+  modules: ["@nuxt/fonts", "vuetify-nuxt-module"],
 
   vuetify: {
     moduleOptions: {
